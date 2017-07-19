da87ec589a876cb6013d4aad00c2e54e00069515
steafan <244007461@qq.com>
2017-07-19 21:30:04 +0800

    project init commit

---
+++ src/main/java/com/univmall/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.univmall.dao;
+
+import com.univmall.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
